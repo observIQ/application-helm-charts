ffc2d2677e91a90df4ffb583eac74a548d7d0513
jsirianni <joe.sirianni@observiq.com>
2022-11-18 10:55:10 -0500

    add loadgen container for tensorflow

---
+++ container/tensorflowload/load.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+while :
+do
+    curl -v \
+        -d '{"instances": [13.0, 23.0, 53.0, 103.0, 503.0, 15, 17]}' \
+        -X POST \
+        http://localhost:8501/v1/models/half_plus_two:pedict
+    sleep 10
+done
