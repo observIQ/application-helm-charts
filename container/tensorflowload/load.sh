a44f9ca93705beed2c01bbb1b9f21d5ea6fcd287
jsirianni <joe.sirianni@observiq.com>
2022-11-23 14:22:19 -0500

    hide curl output

--- container/tensorflowload/load.sh
+++ container/tensorflowload/load.sh
@@ -3,6 +3,8 @@
 while :
 do
     curl -v \
+        --silent \
+        --output /dev/null \
         -d '{"instances": [13.0, 23.0, 53.0, 103.0, 503.0, 15, 17]}' \
         -X POST \
         http://localhost:8501/v1/models/half_plus_two:predict
