071f499ca9453ef45ab32912d5eec64edad83884
jsirianni <joe.sirianni@observiq.com>
2022-11-23 08:38:45 -0500

    fix tensorflow loadgen

--- container/tensorflowload/load.sh
+++ container/tensorflowload/load.sh
@@ -5,6 +5,6 @@ do
     curl -v \
         -d '{"instances": [13.0, 23.0, 53.0, 103.0, 503.0, 15, 17]}' \
         -X POST \
-        http://localhost:8501/v1/models/half_plus_two:pedict
+        http://localhost:8501/v1/models/half_plus_two:predict
     sleep 10
 done
