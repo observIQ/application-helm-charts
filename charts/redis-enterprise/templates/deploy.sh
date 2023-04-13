36efacb23e90414710b4c86d353910799ce8740c
Joe Sirianni <joe.sirianni@observiq.com>
2023-04-13 14:58:37 -0400

    deploy script

--- charts/redis-enterprise/templates/deploy.sh
+++ charts/redis-enterprise/templates/deploy.sh
@@ -8,3 +8,9 @@ kubectl apply -f operator-latest.yaml -n redis-0
 kubectl apply -f operator-latest.yaml -n redis-1
 
 kubectl apply -f cluster.yaml
+
+kubectl apply -f ingress/cert-manager.yaml
+kubectl apply -f ingress/nginx.yaml
+#sleep 30
+kubectl apply -f ingress/issuer.yaml
+kubectl apply -f ingress/ingress.yaml
